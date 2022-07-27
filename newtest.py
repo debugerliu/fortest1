abd4d37398edda4168b4e187d2448afc181ddfb2
刘兴文 <719918768@qq.com>
2022-07-27 14:06:11 +0800

    我在本地写了我在这里修改了

--- newtest.py
+++ newtest.py
@@ -7,7 +7,7 @@ class TestClass:
         assert "h" in x
 
     def test_three(self):
-        x = "hello本地"
+        x = "hello真的本地"
         # 我这里修改了
         assert hasattr(x, "check")
         # 本地修改了
