a7d7cefa8e691c6e41d3d1b2f7a0f47af95926b6
debugerliu <52487133+debugerliu@users.noreply.github.com>
2022-08-03 09:14:17 +0800

    Update newtest.py
    别人提交

--- newtest.py
+++ newtest.py
@@ -9,7 +9,7 @@ class TestClass:
     def test_three(self):
         x = "lxw远程2"
         # 我这里修改了
-        assert hasattr(x, "check")
+        assert hasattr(x, "check")111
         # 本地修改了
         # 线上修改
 
