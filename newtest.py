70b75b5936334249a62dfc94c625a2b609c806cb
刘兴文 <719918768@qq.com>
2022-11-11 19:39:40 +0800

    dev分支提交了更改

--- newtest.py
+++ newtest.py
@@ -1,14 +1,1 @@
-# import random
-#
-# list = [random.randint(0, 6) for i in range(6)]
-# print(list)
-
-import random
-
-list = random.sample([i for i in range(0, 7)], 7)
-print(list)
-
-# 11.11我在dev分支提交了代码
-# 我在master分支提交了代码
-
-# 我在dev提交了代码
+# 我在dev环境
