e6c2c2b75e74e725c3b5e59f87448ef96aa576fa
刘兴文 <719918768@qq.com>
2022-11-24 09:19:56 +0800

    github的host优化脚本

--- githubinfo.py
+++ githubinfo.py
@@ -10,7 +10,6 @@ import requests
 """
 
 header = {
-    'Host': 'ipaddress.com',
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36',
     'Referer': 'https://www.ipaddress.com/',
 }
